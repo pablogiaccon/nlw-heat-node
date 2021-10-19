ccff8e557b289d79d4e3039d8ac50c4d53ed817a
Pablo Giaccon <psjacon1995@gmail.com>
2021-10-18 22:20:08 -0300

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
+
+const router = Router();
+
+router.post("/authenticate", new AuthenticateUserController().handle);
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
+router.post('/messages', ensureAuthenticated, new CreateMessageController().handle)
+router.get('/messages/last3', ensureAuthenticated, new GetLast3MessagesController().handle)
+
+export { router };
